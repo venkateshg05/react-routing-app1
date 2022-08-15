fb6907e0b5385b498554aa65f8968fa7366df929
venkateshg05 <venkatesh_gopal@outlook.com>
2022-08-15 02:45:04 -0500

    (feat) fetch quote details from db

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,30 +1,52 @@
-import { Fragment } from "react";
-import { Link, Route, useParams, useRouteMatch } from "react-router-dom";
-import Comments from "../components/comments/Comments";
+import { Fragment, useEffect } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
-import NoQuotesFound from "../components/quotes/NoQuotesFound";
+import Comments from "../components/comments/Comments";
+import useHttp from "../hooks/use-http";
+import { getSingleQuote } from "../lib/api";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
 
-const DUMMY_QUOTES = [
-  { id: "q1", author: "Max", text: "Learning React is fun!" },
-  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
-];
-
-const QuotesDetails = () => {
+const QuoteDetail = () => {
   const match = useRouteMatch();
   const params = useParams();
-  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
 
-  if (!quote) {
-    return <NoQuotesFound />;
+  const { quoteId } = params;
+
+  const {
+    sendRequest,
+    status,
+    data: loadedQuote,
+    error,
+  } = useHttp(getSingleQuote, true);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  if (status === "pending") {
+    return (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (error) {
+    return <p className="centered">{error}</p>;
+  }
+
+  if (!loadedQuote.text) {
+    return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
-      <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`${match.path}`} exact>
+      <HighlightedQuote text={loadedQuote.text} author={loadedQuote.author} />
+      <Route path={match.path} exact>
         <div className="centered">
           <Link className="btn--flat" to={`${match.url}/comments`}>
-            Load comments
+            Load Comments
           </Link>
         </div>
       </Route>
@@ -35,4 +57,4 @@ const QuotesDetails = () => {
   );
 };
 
-export default QuotesDetails;
+export default QuoteDetail;
