808f2f52b507a65b85e54bfb3177fa5200bbc6e9
venkateshg05 <venkatesh_gopal@outlook.com>
2022-08-15 01:18:48 -0500

    (feat) send users to all quotes after submit new quote

--- src/pages/AddQuote.js
+++ src/pages/AddQuote.js
@@ -1,8 +1,11 @@
+import { useHistory } from "react-router-dom";
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const AddQuotes = () => {
+  const history = useHistory();
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+    history.replace("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
