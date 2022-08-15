594dffbcf674bbcc1cfb5ba478f156c4a598cba8
venkateshg05 <venkatesh_gopal@outlook.com>
2022-08-15 00:42:14 -0500

    (feat) add comments url for each quote detail page

--- src/pages/QuoteDetails.js
+++ src/pages/QuoteDetails.js
@@ -1,9 +1,16 @@
 import { Fragment } from "react";
+import { Route, useParams } from "react-router-dom";
+import Comments from "../components/comments/Comments";
 
 const QuotesDetails = () => {
+  const params = useParams();
   return (
     <Fragment>
       <h1>QuotesDetails</h1>
+      <p>{params.quoteId}</p>
+      <Route path={"/quotes/:quoteId/comments"}>
+        <Comments />
+      </Route>
     </Fragment>
   );
 };
