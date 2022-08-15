b8b9943de1d373906153ea2163001ad55a526c30
venkateshg05 <venkatesh_gopal@outlook.com>
2022-08-15 02:42:15 -0500

    (feat) add new quotes to db

--- src/pages/AddQuote.js
+++ src/pages/AddQuote.js
@@ -1,14 +1,27 @@
+import { useEffect } from "react";
 import { useHistory } from "react-router-dom";
+
 import QuoteForm from "../components/quotes/QuoteForm";
+import useHttp from "../hooks/use-http";
+import { addQuote } from "../lib/api";
 
 const AddQuotes = () => {
+  const { sendRequest, status } = useHttp(addQuote);
   const history = useHistory();
+
+  useEffect(() => {
+    if (status === "completed") {
+      history.push("/quotes");
+    }
+  }, [status, history]);
+
   const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
-    history.replace("/quotes");
+    sendRequest(quoteData);
   };
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+  return (
+    <QuoteForm isLoading={status === "pending"} onAddQuote={addQuoteHandler} />
+  );
 };
 
 export default AddQuotes;
